7a42e790ea14898da0ba5da0a2ef2597a8571281
estelleJoyb <estelle.joyb@gmail.com>
2022-03-28 13:54:19 +0200

    fonction affiche

--- fish.js
+++ fish.js
@@ -1,25 +1,24 @@
+var url = "https://www.fishwatch.gov/api/species/";
+
 function search(){
     var elementrecherche = document.getElementById("zone_recherche").value;
     elementrecherche = elementrecherche.replace(" ","-");
     elementrecherche = elementrecherche.toLowerCase();
     elementrecherche = encodeURIComponent(elementrecherche);
-    alert(elementrecherche);
+    
     affiche(elementrecherche);
 }
 
 function affiche(i){
-    var blocres = document.getElementById("bloc-resultats");
-    if(blocres != null){
-        getElementsByClassName("res").value = i.value;
-    }
-   // crée un nouvel élément div
-  //var newP = document.createElement("p");
-  // et lui donne un peu de contenu
-  //var newContent = document.createTextNode(i);
-  // ajoute le nœud texte au nouveau div créé
-  //newP.appendChild(newContent);
+    // crée un nouvel élément div
+    var newp = document.createElement("p");
+    // et lui donne un peu de contenu
+    var newContent = document.createTextNode(i);
+    // ajoute le nœud texte au nouveau div créé
+    newp.appendChild(newContent);
 
-  // ajoute le nouvel élément créé et son contenu dans le DOM
-  //var currentP = document.getElementById('res');
-  //document.getElementById("bloc-resultats").insertBefore(newP, currentP);
-  }
+    // ajoute le nouvel élément créé et son contenu dans le DOM
+    var currentp = document.getElementById('res');
+    document.getElementById("bloc-resultats").insertBefore(newp, currentp);
+    newp.classList.add("res");
+}
