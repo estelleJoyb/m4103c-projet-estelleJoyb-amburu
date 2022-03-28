47ed9de42b452b70bd6185d20c7f1c769f906ad4
estelleJoyb <estelle.joyb@gmail.com>
2022-03-28 12:39:15 +0200

    search

--- fish.js
+++ fish.js
@@ -1,8 +1,8 @@
 function search(){
     //var elementrecherche = $(#zone_recherche).val;
     var elementrecherche = document.getElementById("zone_recherche").value;
-    elementrecherche = encodeURIComponent(elementrecherche);
     elementrecherche = elementrecherche.replace(" ","-");
     elementrecherche = elementrecherche.toLowerCase();
+    elementrecherche = encodeURIComponent(elementrecherche);
     alert(elementrecherche);
 }
