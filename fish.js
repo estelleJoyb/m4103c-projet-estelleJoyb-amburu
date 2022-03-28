7d96348e705fdd44a0253583fd655f495241107e
estelleJoyb <estelle.joyb@gmail.com>
2022-03-28 13:08:59 +0200

    test

--- fish.js
+++ fish.js
@@ -1,8 +1,25 @@
 function search(){
-    //var elementrecherche = $(#zone_recherche).val;
     var elementrecherche = document.getElementById("zone_recherche").value;
     elementrecherche = elementrecherche.replace(" ","-");
     elementrecherche = elementrecherche.toLowerCase();
     elementrecherche = encodeURIComponent(elementrecherche);
     alert(elementrecherche);
+    affiche(elementrecherche);
 }
+
+function affiche(i){
+    var blocres = document.getElementById("bloc-resultats");
+    if(blocres != null){
+        getElementsByClassName("res").value = i.value;
+    }
+   // crée un nouvel élément div
+  //var newP = document.createElement("p");
+  // et lui donne un peu de contenu
+  //var newContent = document.createTextNode(i);
+  // ajoute le nœud texte au nouveau div créé
+  //newP.appendChild(newContent);
+
+  // ajoute le nouvel élément créé et son contenu dans le DOM
+  //var currentP = document.getElementById('res');
+  //document.getElementById("bloc-resultats").insertBefore(newP, currentP);
+  }
