c45cb644fa78055db1c69834ffddb0735dc520ca
joyeuxbe <estelle.joyeux-bouillon@etu.univ-grenoble-alpes.fr>
2022-03-31 14:39:03 +0200

    search v3

--- fish.js
+++ fish.js
@@ -8,7 +8,7 @@ var ilyafav;
 var url;
 
 function search(){
-    document.getElementsByClassName('#bloc-resultat').innerHTML = "";
+    document.getElementsByClassName('#bloc-resultat').innerHTML = " ";
     elementrecherche = document.getElementById("zone_recherche").value;
     elementrecherche = elementrecherche.replace(" ","-");
     elementrecherche = elementrecherche.toLowerCase();
