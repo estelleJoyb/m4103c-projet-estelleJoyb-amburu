22bb90649629a3d33cdd0f05a683d021db9b0031
estelleJoyb <estelle.joyb@gmail.com>
2022-03-31 12:22:36 +0200

    favoris

--- fish.js
+++ fish.js
@@ -2,17 +2,25 @@ var proxyUrl ="https://api.allorigins.win/get?url=";
 var apiUrl = "https://www.fishwatch.gov/api/species/";
 var Data;
 var blocResultat = document.querySelector('#bloc-resultats');
-var blocFav = document.getElementById("section-favoris");
+var blocFav = document.getElementById("liste-favoris");
 var elementrecherche;
 var ilyafav;
+var url;
 
 function search(){
+    blocResultat.value ="";
     elementrecherche = document.getElementById("zone_recherche").value;
     elementrecherche = elementrecherche.replace(" ","-");
     elementrecherche = elementrecherche.toLowerCase();
     elementrecherche = encodeURIComponent(elementrecherche);
-    var url = proxyUrl+encodeURIComponent(apiUrl+elementrecherche);
+    url = proxyUrl+encodeURIComponent(apiUrl+elementrecherche);
+    
     loadJSON(url, myData,'jsonp');
+}
+
+function rechercheByFav(name){
+  var barre = document.getElementById("zone_recherche");
+  barre.value = name;
 }
 
 function affiche(i,nom){
@@ -25,8 +33,9 @@ function afficheFav(i){
   var leLi = document.createElement("li");
   blocFav.appendChild(leLi);
   var valeur = document.createElement("span");
-  valeur.innerHTML = i;
+  valeur.innerHTML = i+" ";
   leLi.appendChild(valeur);
+  leLi.innerHTML += "<img src=\"images/croix.svg\" alt=\"Icone pour supprimer le favori\" width=15 title=\"Cliquer pour supprimer le favori\">";
   valeur.title = title="Cliquer pour relancer la recherche";
 }
 
@@ -63,7 +72,6 @@ function loadJSON(path, success, error) {
     var resultat2 = JSON.parse(resultat.contents);
 
     for(var res of resultat2){
-      //if(res["Species Name"] == elementrecherche) {
         affiche(res["Species Name"]+ " - " + res["Scientific Name"], "name");
         affiche("Habitat :" +res["Habitat"], "habitat");
         afficheImage(res["Species Illustration Photo"].src, "illu");
@@ -80,7 +88,9 @@ function loadJSON(path, success, error) {
       if(elementrecherche == undefined){
         alert("Vous n'avez pas encore fait de recherche !");
       }else{
-        affiche(elementrecherche,"span",)
+        afficheFav(elementrecherche,"span");
+        console.log(url);
+        setCookie(elementrecherche, url);
       }
     }else{
       elem.setAttribute('src','images/etoile-vide.svg');
