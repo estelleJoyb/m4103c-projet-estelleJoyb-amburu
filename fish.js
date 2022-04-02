40031a347eba983e3a2d1ec34f297753de0ab464
joyeuxbe <estelle.joyeux-bouillon@etu.univ-grenoble-alpes.fr>
2022-04-02 17:43:32 +0200

    search V2

--- fish.js
+++ fish.js
@@ -18,11 +18,11 @@ function search(){
     elementrecherche = encodeURIComponent(elementrecherche);
     url = proxyUrl+encodeURIComponent(apiUrl+elementrecherche);
     
-    if(localStorage.getItem(elementrecherche)==url){//alors on l'a en favori
-      if(etoile.getAttribute('src') == "images/etoile-vide.svg"){
-        etoile.setAttribute('src','images/etoile-pleine.svg');
-        etoile.setAttribute('alt','Etoile Pleine');}
-    }
+    // if(localStorage.getItem(elementrecherche)==url){//alors on l'a en favori
+    //   if(etoile.getAttribute('src') == "images/etoile-vide.svg"){
+    //     etoile.setAttribute('src','images/etoile-pleine.svg');
+    //     etoile.setAttribute('alt','Etoile Pleine');}
+    // }
     loadJSON(url, myData,'jsonp');
 }
 
