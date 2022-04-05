8469c8b79f165a9a38f2a46c610e0dda0c7828d8
Ambre <ambrenguy@gmail.com>
2022-04-05 11:51:53 +0200

    recherche by fav

--- fish.js
+++ fish.js
@@ -8,10 +8,39 @@ var fav = document.querySelector("#btn-favoris");
 var etoile = document.getElementById("etoile");
 var blocFav = document.getElementById("liste-favoris");
 var listeFav = new Array();
-var poissonLi;
 
 function onLoad(){
   fav.onclick=function(){favoris()};
+  var contenuFav = localStorage.getItem('favoris');
+  if(contenuFav == null || contenuFav == ""){
+    localStorage.setItem('favoris', JSON.stringify([]));
+  } else {
+    listeFav = JSON.parse(contenuFav);
+  }
+  afficheFavoris();
+  var zoneRecherche = document.getElementById("zone_recherche");
+  zoneRecherche.addEventListener('keyup', function() {majEtoile(this.value)});
+}
+
+function majEtoile(elem){
+  console.log(elem);
+  if(elem == ""){
+    //si la barre de recherche est vide
+    etoile.setAttribute('src','images/etoile-vide.svg');
+    etoile.setAttribute('alt','Etoile Vide');
+    etoile.setAttribute('disabled', true);
+  } else {
+    elem = elem.replace(/ /g,"-");
+    elem = elem.toLowerCase();
+    if(listeFav.indexOf(elem) != -1){
+      etoile.setAttribute('src','images/etoile-pleine.svg');
+      etoile.setAttribute('alt','Etoile Pleine');
+      etoile.removeAttribute('disabled');
+    } else {
+      etoile.setAttribute('src','images/etoile-vide.svg');
+      etoile.setAttribute('alt','Etoile Vide');
+    }
+  }
 }
 
 function search(){
@@ -21,11 +50,18 @@ function search(){
     elementRecherche = encodeURIComponent(elementRecherche);
     url = proxyUrl+encodeURIComponent(apiUrl+elementRecherche);
     loadJSON(url, myData,'jsonp');
-    for(i = 0; i < listeFav.length; i++){
-      if(elementRecherche != listeFav[i]){
-        etoile.setAttribute('src','images/etoile-vide.svg');
-        etoile.setAttribute('alt','Etoile Vide');
-      }
+    // for(i = 0; i < listeFav.length; i++){
+    //   if(elementRecherche != listeFav[i]){
+    //     etoile.setAttribute('src','images/etoile-vide.svg');
+    //     etoile.setAttribute('alt','Etoile Vide');
+    //   }
+    // }
+    if(listeFav.indexOf(elementRecherche) != -1){
+      etoile.setAttribute('src','images/etoile-pleine.svg');
+      etoile.setAttribute('alt','Etoile Pleine');
+    } else {
+      etoile.setAttribute('src','images/etoile-vide.svg');
+      etoile.setAttribute('alt','Etoile Vide');
     }
     if(blocResultat != null){
       blocResultat.innerHTML = " ";
@@ -79,35 +115,38 @@ function loadJSON(path, success, error) {
         
     }
   }
-//D'après Lori :
-//faire fichier JSON ou on stok array
 
-//localStorage.setItem("favori",stringify(objJSON))
   function favoris(){
+    //lorsqu'on clique sur l'étoile
+    if(elementRecherche == null){
+      return;
+    }
     if(listeFav.length === 0){
       //si il n'y a pas de poisson en favoris
       etoile.setAttribute('src','images/etoile-pleine.svg');
       etoile.setAttribute('alt','Etoile Pleine');
-      localStorage.setItem(elementRecherche,elementRecherche);
       listeFav[0] = elementRecherche;
-      ajouteFav(elementRecherche);
-    } else if (localStorage.getItem(elementRecherche) == elementRecherche){
+      localStorage.setItem('favoris', JSON.stringify(listeFav));
+      afficheFavoris();
+      //ajouteFav(elementRecherche);
+    } else if (listeFav.indexOf(elementRecherche) != -1){
       //si le poisson actuel est déjà en favoris
       etoile.setAttribute('src','images/etoile-vide.svg');
       etoile.setAttribute('alt','Etoile Vide');
-      localStorage.removeItem(elementRecherche,elementRecherche);
       suppFav(elementRecherche);
+      afficheFavoris();
     } else {
       //si il y a déjà des favoris mais pas le poisson actuel
       etoile.setAttribute('src','images/etoile-pleine.svg');
       etoile.setAttribute('alt','Etoile Pleine'); 
-      localStorage.setItem(elementRecherche,elementRecherche);
       listeFav.push(elementRecherche);
-      ajouteFav(elementRecherche);
+      localStorage.setItem('favoris', JSON.stringify(listeFav));
+      afficheFavoris();
+      //ajouteFav(elementRecherche);
     }
   }
 
-  function ajouteFav(elem){
+  function afficheFavoris(){
     if(listeFav.length == 0){
     //si la liste des favoris est vide
       var valeur = document.createElement("p");
@@ -117,7 +156,7 @@ function loadJSON(path, success, error) {
       blocFav.innerHTML = " ";
         for(var i = 0; i < listeFav.length; i++){
             //Pour chaque élément de listeFav, on crée un élément li poissonLi de classe unFavori
-            poissonLi = document.createElement("li");
+            const poissonLi = document.createElement("li");
             poissonLi.classList.add("unFavori");
 
             //On crée un élément span poissonActuel et une imageCroix de suppression dans chaque li
@@ -128,7 +167,6 @@ function loadJSON(path, success, error) {
             poissonActuel.innerHTML = listeFav[i];
             const contenuspan = new String(listeFav[i]);//const car sinon ça prends toujours la dernière valeur de listFav
             poissonActuel.onclick=function(){
-              console.log(contenuspan);
               recherchebyfav(contenuspan);
             }
             imageCroix.src = "images/croix.svg";
@@ -136,12 +174,9 @@ function loadJSON(path, success, error) {
             imageCroix.title = "Cliquez pour supprimer le favori";
             imageCroix.width = "15";
             imageCroix.classList.add("croix");
-
             imageCroix.onclick=function(){
-              poissonLi.remove();
               suppFav(contenuspan);
             };
-
             poissonLi.appendChild(poissonActuel);
             poissonLi.appendChild(imageCroix);
             blocFav.appendChild(poissonLi);
@@ -153,11 +188,19 @@ function loadJSON(path, success, error) {
     elementRecherche = document.getElementById("zone_recherche").value = elem;
     url = proxyUrl+encodeURIComponent(apiUrl+elementRecherche);
     loadJSON(url, myData,'jsonp');
+    
     for(i = 0; i < listeFav.length; i++){
       if(elementRecherche != listeFav[i]){
         etoile.setAttribute('src','images/etoile-vide.svg');
         etoile.setAttribute('alt','Etoile Vide');
       }
+    }
+    if(listeFav.indexOf(elementRecherche) != -1){
+      etoile.setAttribute('src','images/etoile-pleine.svg');
+      etoile.setAttribute('alt','Etoile Pleine');
+    } else {
+      etoile.setAttribute('src','images/etoile-vide.svg');
+      etoile.setAttribute('alt','Etoile Vide');
     }
     if(blocResultat != null){
       blocResultat.innerHTML = " ";
@@ -165,14 +208,21 @@ function loadJSON(path, success, error) {
   }
 
   function suppFav(elem){
-    localStorage.removeItem(elem);
-    for(i = 0; i <= listeFav.length; i++){
-      if(listeFav[i]==elem){
-        listeFav.splice(i,1);
-        poissonLi.remove();
-        etoile.setAttribute('src','images/etoile-vide.svg');
-        etoile.setAttribute('alt','Etoile Vide');
-      }
+    // localStorage.removeItem(elem);
+    // for(i = 0; i <= listeFav.length; i++){
+    //   if(listeFav[i]==elem){
+    //     
+    //     poisson.remove();
+    //     etoile.setAttribute('src','images/etoile-vide.svg');
+    //     etoile.setAttribute('alt','Etoile Vide');
+    //   }
+    // }
+    elem = elem.toString();
+    var index = listeFav.indexOf(elem);
+    if(index != -1){
+      listeFav.splice(index,1);
+      localStorage.setItem('favoris', JSON.stringify(listeFav));
+      afficheFavoris();
     }
   }
 
